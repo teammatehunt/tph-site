139ea52ad589d03c0d890b9e2e377acdf0d7f497
Ivan Wang <ivan.wang@flyzipline.com>
2023-10-19 12:46:04 -0700

    Add back individual registration

--- reg-client/components/register_individual.tsx
+++ reg-client/components/register_individual.tsx
@@ -9,6 +9,9 @@ import { RegistrationClosed as IndividualRegistrationClosed } from 'components/i
 import { DjangoFormResponse, DjangoFormErrors } from 'types';
 import HuntEmail from './hunt_email';
 
+// TODO: Update to check hunt start date.
+const IS_REGISTRATION_CLOSED = false;
+
 // Represents a server.spoilr.registration.RegistrationInfo + user
 interface RegisterIndividualForm {
   username?: string; // For user creation during initial registration only
@@ -127,7 +130,7 @@ const RegisterIndividual = ({
     }
   };
 
-  if (!isLoggedIn) return <RegistrationClosed />;
+  if (!isLoggedIn && IS_REGISTRATION_CLOSED) return <RegistrationClosed />;
 
   return (
     <>
@@ -168,7 +171,178 @@ const RegisterIndividual = ({
             </Link>{' '}
             not this one.
           </p>
-          <IndividualRegistrationClosed />
+          {IS_REGISTRATION_CLOSED ? (
+            <IndividualRegistrationClosed />
+          ) : (
+            <p>
+              You only need to fill out required fields (
+              <span className="text-red-500">*</span>) at minimum, and you will
+              have an opportunity to provide updated information in the future
+              by logging in. You can omit any questions whose answers you don't
+              know yet or just fill them in to the best of your ability.
+            </p>
+          )}
+        </>
+      )}
+      {!IS_REGISTRATION_CLOSED && (
+        <>
+          <p>
+            If you have further questions about registration, feel free to email
+            us at <HuntEmail />.
+          </p>
+          <form
+            onSubmit={onSubmit}
+            onKeyDown={(e) => {
+              // Disable Enter from submitting form by accident
+              if (e.code === 'Enter') e.preventDefault();
+            }}
+          >
+            {isLoading ? (
+              <div>Loading your registration…</div>
+            ) : (
+              <div className="flex flex-col space-y-16">
+                <section>
+                  <FormRow
+                    name="contact_first_name"
+                    label="First (Given) name"
+                    errors={errors.contact_first_name}
+                    defaultValue={registrationInfo?.contact_first_name}
+                    required
+                    autofocus={!isLoggedIn}
+                  />
+                  <FormRow
+                    name="contact_last_name"
+                    label="Last (Family) name"
+                    errors={errors.contact_last_name}
+                    defaultValue={registrationInfo?.contact_last_name}
+                    required
+                  />
+                  <FormRow
+                    name="contact_email"
+                    label="Email address"
+                    errors={errors.contact_email}
+                    defaultValue={registrationInfo?.contact_email}
+                    required
+                  />
+
+                  {!isLoggedIn && (
+                    <>
+                      <FormRow
+                        name="username"
+                        label="Username"
+                        info="Only for editing your registration. If we're able to match you with a team, the team will have a shared username and password for accessing Hunt."
+                        errors={errors.username}
+                        required
+                      />
+                      <FormRow
+                        name="password1"
+                        label="Password"
+                        type="password"
+                        errors={errors.password1}
+                        required
+                      />
+                      <FormRow
+                        name="password2"
+                        label="Confirm Password"
+                        type="password"
+                        errors={errors.password2}
+                        required
+                      />
+                    </>
+                  )}
+                </section>
+
+                <section>
+                  <h3>Background</h3>
+                  <div>
+                    Help us understand your preferences so we can match you with
+                    a team better.
+                  </div>
+                  <FormRow
+                    name="bg_mh_history"
+                    label="Have you participated in the MIT Mystery Hunt before? If so, tell us about your Hunt history!"
+                    info="(500 character limit)"
+                    lines={3}
+                    defaultValue={registrationInfo?.bg_mh_history}
+                    errors={errors.bg_mh_history}
+                  />
+                  <FormRow
+                    name="bg_other_history"
+                    label="Have you played in other puzzle-type events before? If so, tell us about it!"
+                    info="(500 character limit)"
+                    lines={3}
+                    defaultValue={registrationInfo?.bg_other_history}
+                    errors={errors.bg_other_history}
+                  />
+                  <FormRow
+                    name="bg_playstyle"
+                    label="What describes your style of playing?"
+                    options={{
+                      fun: "I'm just playing for fun and would like to make some new friends.",
+                      puzzles:
+                        "I'm pretty into puzzles, but not so focused on winning.",
+                      win: "I'm a puzzle machine, and I'd love to be on a team that wants to find the coin. I really want to see all of Hunt.",
+                    }}
+                    defaultValue={registrationInfo?.bg_playstyle}
+                    errors={errors.bg_playstyle}
+                  />
+                  <FormRow
+                    name="bg_other_prefs"
+                    label="Do you have any other preferences about what team you want to join?"
+                    info="For example, larger or smaller teams, teams of current students or alumni/older players, etc."
+                    defaultValue={registrationInfo?.bg_other_prefs}
+                    errors={errors.bg_other_prefs}
+                  />
+                  <FormRow
+                    name="bg_on_campus"
+                    label="Do you plan to participate on campus?"
+                    options={{
+                      yes: "Yes, I'll be on campus",
+                      maybe: 'Maybe',
+                      no: "No, I'll participate remotely",
+                    }}
+                    defaultValue={registrationInfo?.bg_on_campus}
+                    errors={errors.bg_on_campus}
+                  />
+                  <FormRow
+                    name="bg_under_18"
+                    label="Are you under 18?"
+                    info="Note: Non-MIT minors must be accompanied by a parent or guardian at all times, and may not attend kickoff, events, interactions away from their team’s HQ, or field puzzles."
+                    options={{
+                      yes: 'Yes',
+                      no: 'No',
+                    }}
+                    defaultValue={registrationInfo?.bg_under_18}
+                    errors={errors.bg_under_18}
+                  />
+                  <FormRow
+                    name="bg_mit_connection"
+                    label="Are you connected to the MIT community? If so, how?"
+                    defaultValue={registrationInfo?.bg_mit_connection}
+                    errors={errors.bg_mit_connection}
+                  />
+                  <FormRow
+                    name="other"
+                    label="Anything else you'd like to share with us?"
+                    info="Comments, questions, puns?"
+                    lines={3}
+                    defaultValue={registrationInfo?.other}
+                    errors={errors.other}
+                  />
+                </section>
+
+                <div className="submit text-center">
+                  <button type="submit" disabled={isSubmitting}>
+                    {isLoggedIn ? 'Update registration' : 'Submit'}
+                  </button>
+                </div>
+
+                {errors.__all__ && (
+                  <p className="formerror">{errors.__all__}</p>
+                )}
+              </div>
+            )}
+          </form>
         </>
       )}
     </>
